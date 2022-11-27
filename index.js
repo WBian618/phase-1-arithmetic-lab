af229f5f645476891b203fb8b8d14ab096ec76c8
webster <wbian618@gmail.com>
2022-11-26 20:31:45 -0500

    commit tree c74f72ad6e5d4f7ab6d715cf57f365e213f8b291

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
 
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(5,9,14,18,19,20)
 // Write your code here
